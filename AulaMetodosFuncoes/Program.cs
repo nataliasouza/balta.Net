00bb7e34437696afcb691b49356a062f9f794f89
nataliasouza <nataliasouza.ti@hotmail.com>
2021-03-27 21:03:34 -0300

    Aula de Metódos e Funções

---
+++ AulaMetodosFuncoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+
+            string nome = RetornaNome("Nat", "Souza");
+            Console.WriteLine(nome);
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é legal!");
+        }
+
+        static string RetornaNome( 
+            string nome, 
+            string sobrenome,
+            int idade = 36
+        )
+        { 
+            return nome +" "+ sobrenome + " tem " + idade.ToString();
+        }
+    }
+}
