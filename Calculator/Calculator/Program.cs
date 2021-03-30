e74f5bd2db04137111e6e02c4bbe6e3c4993a453
nataliasouza <nataliasouza.ti@hotmail.com>
2021-03-30 17:47:08 -0300

    Aula Subtraçao concluída

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,6 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Subtracao();
+        }
+
+        static void Soma()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -18,10 +23,27 @@ namespace Calculator
 
             float resultado = v1 + v2;
             //Formas de mostrar os resultados 
-            Console.WriteLine("O resultado da soma é: " + resultado);
+            //Console.WriteLine("O resultado da soma é: " + resultado);
             Console.WriteLine($"O resultado da soma é: { resultado}");
-            Console.WriteLine($"O resultado da soma é: {v1 + v2}");
-            Console.WriteLine("O resultado da soma é: " + (v1 + v2));
+            //Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+            //Console.WriteLine("O resultado da soma é: " + (v1 + v2)); 
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é: {resultado}" );
+            Console.ReadKey();
         }
     }
 }
