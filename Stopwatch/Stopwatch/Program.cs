f4e4fbf34215525cb5024ee440873f78e130fa8c
nataliasouza <nataliasouza.ti@hotmail.com>
2021-03-30 21:56:30 -0300

    Thread e Sleep

--- Stopwatch/Stopwatch/Program.cs
+++ Stopwatch/Stopwatch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Stopwatch
 {
@@ -6,7 +7,7 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
         }
         static void Start()
         {
@@ -15,7 +16,10 @@ namespace Stopwatch
 
             while(currentTime != time)
             {
+                Console.Clear();
                 currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
             }
         }
     }
