dd7d190ac419366f25413824909be5ba7b536792
nataliasouza <nataliasouza.ti@hotmail.com>
2021-03-31 22:37:29 -0300

    Cronômetro Finalizado

--- Stopwatch/Stopwatch/Program.cs
+++ Stopwatch/Stopwatch/Program.cs
@@ -7,8 +7,40 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start(6);
+            Menu();
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length -1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+        }
+
         static void Start(int time)
         {
             int currentTime = 0;
@@ -23,6 +55,7 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado.");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
