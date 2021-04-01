0610620d113300e7c3a26ae94b207cdb9d5f55cf
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-01 17:52:19 -0300

    Função Salvar - Editor finalizado

--- TextEditor/TextEditor/Program.cs
+++ TextEditor/TextEditor/Program.cs
@@ -42,7 +42,18 @@ namespace TextEditor
 
         static void Open()
         {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
 
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Edit()
