332ad1f974a7f4200d58dbfd5f3910d60bf4a0ad
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-01 16:37:19 -0300

    StreamWriter

--- TextEditor/TextEditor/Program.cs
+++ TextEditor/TextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -62,6 +63,18 @@ namespace TextEditor
             while (Console.ReadKey().Key != ConsoleKey.Escape);
 
             Console.Write(text);
+        }
+
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
         }
     }
 }
