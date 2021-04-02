77c55ff0235fc6d3938b66ec6cbf855b66e95f7a
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-02 19:55:40 -0300

    Contains - CompareTo

---
+++ ComparacaoStrings/ComparacaoStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ComparacaoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "É apenas um teste";
+            Console.WriteLine(texto.CompareTo("É apenas um teste"));
+            Console.WriteLine(texto.Contains("Teste"));
+            Console.WriteLine(texto.Contains("teste"));
+            Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
