c71ad0227cd318446fd000db9e99a27b6cee65d2
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-02 13:42:52 -0300

    Aula Strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("a040434d-5efe-4a74-8c03-89313d5a638f");
+
+            if(id == Guid.NewGuid())
+            Console.WriteLine(id.ToString().Substring(0,8));
+        }
+    }
+}
