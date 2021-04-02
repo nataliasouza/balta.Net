90c8794e887d446a4ed90ecf9b891e25bc39b0ad
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-02 13:51:06 -0300

    Revert "Função Salvar - Editor finalizado"
    This reverts commit 0610620d113300e7c3a26ae94b207cdb9d5f55cf.

--- TextEditor/TextEditor/Program.cs
+++ TextEditor/TextEditor/Program.cs
@@ -42,18 +42,7 @@ namespace TextEditor
 
         static void Open()
         {
-            Console.Clear();
-            Console.WriteLine("Qual o caminho do arquivo?");
-            string path = Console.ReadLine();
 
-            using (var file = new StreamReader(path))
-            {
-                string text = file.ReadToEnd();
-                Console.WriteLine(text);
-            }
-            Console.WriteLine("");
-            Console.ReadLine();
-            Menu();
         }
 
         static void Edit()
