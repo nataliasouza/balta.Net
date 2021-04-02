b66a9d52cb9fb5bfb0befcd4fd2f3b94b75f437e
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-02 20:20:13 -0300

    StartsWith EndsWith

---
+++ StartsWithEndsWith/StartsWithEndsWith/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StartsWithEndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este é um texto de teste";
+
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este",StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(texto.StartsWith("texto"));
+
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.EndsWith("Teste"));
+            Console.WriteLine(texto.EndsWith("XPTO"));
+        }
+    }
+}
