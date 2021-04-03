477ed6727bf2ba4481628a73df584ba263553d24
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-02 21:15:19 -0300

    MetodosAdicionais

---
+++ MetodosAdicionais/MetodosAdicionais/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MetodosAdicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Um texto de exemplo";
+            Console.WriteLine(texto.ToLower());
+            Console.WriteLine(texto.ToUpper());
+            Console.WriteLine(texto.Insert(8, " AQUI"));
+            Console.WriteLine(texto.Remove(3,5));
+            Console.WriteLine(texto.Length);
+        }
+    }
+}
