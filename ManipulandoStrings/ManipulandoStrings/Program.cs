2beec7aff102f95a94f17f46ece0d06b52e941fe
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-02 21:51:03 -0300

    ManipulandoStrings

---
+++ ManipulandoStrings/ManipulandoStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ManipulandoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = " Este texto é um teste ";
+            Console.WriteLine(texto.Replace(" ","-"));
+
+            var divisao = texto.Split(" ");
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+            Console.WriteLine(divisao[3]);
+
+            var resultado = texto.Substring(5,5);
+            Console.WriteLine(resultado);
+
+            Console.WriteLine(texto.Trim());
+        }
+    }
+}
