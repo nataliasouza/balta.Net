482c94880cb9233aa53b88b2c2bacede6f58aaaf
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-13 08:30:53 -0300

    Datas

---
+++ Datas/Datas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Datas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var data = new DateTime(1984, 12, 01, 10, 37, 07);
+            //var data = DateTime.Now;
+            Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
+            Console.WriteLine(data.Minute);
+            Console.WriteLine(data.DayOfWeek);
+        }
+    }
+}
