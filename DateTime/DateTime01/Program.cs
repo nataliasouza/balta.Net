d174fd5c8d4214c2a87fd90eeb8fe195d510f611
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-13 08:54:37 -0300

    DateTime com formatação

---
+++ DateTime/DateTime01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DateTime01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var data = DateTime.Now;
+
+            //var formatada = String.Format("{0}", data);
+            //var formatada = String.Format("{0:yyyy}", data);
+            //var formatada = String.Format("{0:M}", data);
+            //var formatada = String.Format("{0:dd/MM/yyyy}", data);
+            var formatada = String.Format("{0:hh:mm:ss}", data);
+            Console.WriteLine(formatada);
+        }
+    }
+}
