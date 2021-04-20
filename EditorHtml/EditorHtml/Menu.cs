bc5eb7fd06e26e745670ef31a76fcdbc59fd5504
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-19 21:37:12 -0300

    Editor Html com função e Menu

--- EditorHtml/EditorHtml/Menu.cs
+++ EditorHtml/EditorHtml/Menu.cs
@@ -11,15 +11,13 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
         public static void DrawScreen()
         {
-            Console.Write("+");
-            for (int i = 0; i <= 30; i++)
-                    Console.Write("-");
-
-            Console.Write("+");
-            Console.Write("\n");
+            TracosMenu();
 
             for (int lines = 0; lines <=10; lines++)
             {
@@ -31,12 +29,35 @@ namespace EditorHtml
                 Console.Write("\n");
             }
 
+            TracosMenu();
+        }
+
+        public static void TracosMenu()
+        {
             Console.Write("+");
             for (int i = 0; i <= 30; i++)
                 Console.Write("-");
 
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("===========");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("3 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
         }
     }
 }
